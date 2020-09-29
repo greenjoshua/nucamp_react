57671562fb28e4f8f298ee5d4e3d703017594849
Joshua Green <joshuag331@gmail.com>
2020-09-29 05:14:59 -0400

    Redux Actions and Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -32,7 +32,7 @@ function RenderComments({comments}) {
                             --{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         </p>
                     </div>)}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>
         );
     }
@@ -55,7 +55,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row" >
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -91,9 +95,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        alert("Current state is: " + JSON.stringify(values));
-        console.log("Current state is: " + JSON.stringify(values));
-
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -56,13 +56,13 @@ class  Contact extends Component {
                     <div className="col-sm-4">
                         <h5>Our Address</h5>
                         <address>
-                            1 Nucamp Way<br />
-                            Seattle, WA 98001<br />
+                            1 Nucamp Way<br/>
+                            Seattle, WA 98001<br/>
                             U.S.A.
                         </address>
                     </div>
                     <div className="col">
-                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
+                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br/>
                         <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
                     </div>
                 </div>
